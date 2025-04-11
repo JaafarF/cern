01ab6e26fff4c9277870ec1be89184fde609f441
Jaafar Fora <jaafarfora@gmail.com>
2025-04-11 11:01:01 +0100

    SpreadSheet: implement put method

--- src/main/java/com/jaafarfora/cern_exercises/SpreadsheetEngine/SpreadsheetImpl.java
+++ src/main/java/com/jaafarfora/cern_exercises/SpreadsheetEngine/SpreadsheetImpl.java
@@ -1,16 +1,23 @@
 package com.jaafarfora.cern_exercises.SpreadsheetEngine;
 
+import java.util.Arrays;
+
 public class SpreadsheetImpl {
 
-    public SpreadsheetImpl(int rows, int columns) {
+    private String[][] sheet;
 
+    public SpreadsheetImpl(int rows, int columns) {
+        sheet = new String[rows][columns];
+        for (String[] strings : sheet) {
+            Arrays.fill(strings, "");
+        }
     }
 
     public String get(int x, int y) {
-        return "";
+        return this.sheet[x][y];
     }
 
     public void put(int x, int y, String value) {
-
+        this.sheet[x][y] = value;
     }
 }
